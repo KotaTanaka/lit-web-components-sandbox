e266d32ad47f0e2adc7aca9cea68cbca23419b73
KotaTanaka <te11k.tanaka@gmail.com>
2023-04-30 22:30:52 +0900

    feat: MySearchInputのReactComponent化

---
+++ packages/react/src/components/MySearchInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createComponent } from '@lit-labs/react'
+import { MySearchInput as MySearchInputWC } from '@kotice/lit-web-components-sandbox'
+
+export const MySearchInput = createComponent({
+  tagName: 'my-search-input',
+  elementClass: MySearchInputWC,
+  react: React,
+})
