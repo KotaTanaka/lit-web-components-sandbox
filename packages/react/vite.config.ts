4e9501afe646c959f818b65f872888f3eb05ef93
KotaTanaka <te11k.tanaka@gmail.com>
2023-04-30 23:30:52 +0900

    feat: lit-web-components-sandbox-reactのライブラリ化

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/components/index.ts',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
   plugins: [react()],
 })
