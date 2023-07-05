c79d131bbe7c417e6700fb286c5e1a39d2d8c002
KotaTanaka <te11k.tanaka@gmail.com>
2023-07-06 00:21:27 +0900

    fix(client): スタイルを利用側で定義

--- packages/client/src/pages/dnd.tsx
+++ packages/client/src/pages/dnd.tsx
@@ -5,23 +5,25 @@ const DragItem = dynamic(() => import('@/components/DragItem'), { ssr: false });
 const DropArea = dynamic(() => import('@/components/DropArea'), { ssr: false });
 
 const DnDPage: NextPage = () => {
+  const cardClassNames = 'flex items-center w-[240px] h-[48px] px-[16px] py-[8px] rounded-md shadow-md';
+
   return (
     <div className="flex gap-[16px] p-[16px]">
       <div className="flex-1 border p-[16px]">
         <p className="text-center mb-[16px]">Box A</p>
         <DropArea>
-          <DragItem>Item 1</DragItem>
-          <DragItem>Item 2</DragItem>
-          <DragItem>Item 3</DragItem>
-          <DragItem>Item 4</DragItem>
+          <DragItem className={cardClassNames}>Item 1</DragItem>
+          <DragItem className={cardClassNames}>Item 2</DragItem>
+          <DragItem className={cardClassNames}>Item 3</DragItem>
+          <DragItem className={cardClassNames}>Item 4</DragItem>
         </DropArea>
       </div>
       <div className="flex-1 border p-[16px]">
       <p className="text-center mb-[16px]">Box B</p>
         <DropArea>
-          <DragItem>Item 5</DragItem>
-          <DragItem>Item 6</DragItem>
-          <DragItem>Item 7</DragItem>
+          <DragItem className={cardClassNames}>Item 5</DragItem>
+          <DragItem className={cardClassNames}>Item 6</DragItem>
+          <DragItem className={cardClassNames}>Item 7</DragItem>
         </DropArea>
       </div>
     </div>
